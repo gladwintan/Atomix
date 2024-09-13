3040f55bb5a1d4950c9c78881347675176716fea
gladwintan <gladwintan@hotmail.sg>
2024-09-13 20:58:07 +0800

    Update InputTextField component

--- components/InputTextField.tsx
+++ components/InputTextField.tsx
@@ -20,26 +20,30 @@ import {
     containerStyle,
     inputStyle,
     iconStyle,
+    ...props
   }: InputFieldProps) => {
     return (
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
       >
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-          <View className="my-2 w-full">
-            <Text className={`text-lg font-semibold mb-3 ${labelStyle}`}>
+          <View className="w-full my-2">
+            <Text className={`font-semibold mb-2 ml-1 ${labelStyle}`}>
               {label}
             </Text>
             <View
-              className={`flex flex-row justify-start items-center relative bg-neutral-100 rounded-full border border-neutral-100 focus:border-primary-500  ${containerStyle}`}
+              className={`flex flex-row justify-start items-center relative bg-neutral-100 rounded-xl 
+                border border-neutral-100 focus:border-neutral-200  ${containerStyle}`}
             >
               {icon && (
                 <Image source={icon} className={`w-6 h-6 ml-4 ${iconStyle}`} />
               )}
               <TextInput
-                className={`rounded-full p-4 font-semibold text-[15px] flex-1 ${inputStyle} text-left`}
+                className={`rounded-full p-4 flex-1 ${inputStyle} text-left`}
+                placeholder={placeholder}
+                placeholderTextColor="gray"
                 secureTextEntry={secureTextEntry}
-                placeholder={placeholder}              
+                {...props}              
               />
             </View>
           </View>
