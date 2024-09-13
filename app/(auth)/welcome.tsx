063af2dac9cb838e2630db68f90a8b812a46837f
gladwintan <gladwintan@hotmail.sg>
2024-09-13 16:29:45 +0800

    Update onboarding screens

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,10 +1,56 @@
-import { Text, View } from "react-native";
+import { onboarding } from "@/constants/index";
+import { router } from "expo-router";
+import { useRef, useState } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Swiper from "react-native-swiper";
 
 const Onboarding = () => {
+    const swiperRef = useRef<Swiper>(null);
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const isLastSlide = activeIndex === onboarding.length - 1;
+
     return (
-        <SafeAreaView>
-            <Text>Welcome</Text>
+        <SafeAreaView className="flex h-full items-center justify-between bg-white">
+            <TouchableOpacity
+                onPress={() => router.replace("/(auth)/sign-up")}
+                className="w-full flex justify-end items-end p-5"
+            >
+                <Text className="font-bold">Skip</Text>
+            </TouchableOpacity>
+            
+            <Swiper
+                ref={swiperRef}
+                loop={false}
+                dot={<View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />}
+                activeDot={<View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />}
+                onIndexChanged={(index) => setActiveIndex(index)}
+            >
+                {onboarding.map((item) => (
+                     <View key={item.id} className="flex items-center justify-center p-5">
+                        <Image
+                            source={item.image}
+                            className="w-11/12 h-[250px] rounded-lg"
+                            resizeMode="cover"
+                        />
+                        <Text className="text-black font-bold text-2xl text-center mx-10 mt-10">{item.title}</Text>
+                        <Text className="text-slate-600 mx-10 mt-3 text-center">{item.description}</Text>
+
+                    </View>
+                ))}
+            </Swiper>
+
+            <TouchableOpacity 
+                className="bg-[#0286FF] w-11/12"
+                onPress={() => 
+                    isLastSlide 
+                        ? router.replace("/(auth)/sign-up") 
+                        : swiperRef.current?.scrollBy(1)
+                }          
+            >
+                <Text>Next</Text>
+            </TouchableOpacity>
         </SafeAreaView>  
     );
 };
---
+++ constants/index.ts
@@ -0,0 +1,17 @@
+import onboarding1 from "@/assets/images/onboarding1.jpg";
+import onboarding2 from "@/assets/images/onboarding2.jpg";
+
+export const onboarding = [
+    {
+        id: 1, 
+        title: "The perfect way to learn is just a tap away!",
+        description: "The journey begins with Atomix. Start learning effortlessly.",
+        image: onboarding1,
+    },
+    {
+        id: 2, 
+        title: "The perfect way to learn is just a tap away!",
+        description: "Secure an A grade by learning with Atomix. A for Atomix!",
+        image: onboarding2,
+    },
+];
